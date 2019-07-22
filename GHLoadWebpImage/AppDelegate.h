d81e45145a31181f3ab362add9337f2617516691
shabake <45329453@qq.com>
2019-07-23 00:26:42 +0800

    add code

---
+++ GHLoadWebpImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GHLoadWebpImage
+//
+//  Created by zhaozhiwei on 2019/7/22.
+//  Copyright © 2019年 POOP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GHLoadWebpImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHLoadWebpImage
+//
+//  Created by zhaozhiwei on 2019/7/22.
+//  Copyright © 2019年 POOP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
